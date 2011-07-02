21aa328637fb050f8104b14b3f9aabe91603d89d
terry <terrycrosslife@gmail.com>
2011-07-02 11:02:55 +0800

    version 1.06 Added new rich text editor for manage article

--- protected/viewc/article/manage_article.php
+++ protected/viewc/article/manage_article.php
@@ -1,5 +1,6 @@
 <?php $this->renderc('template/head-start'); ?>
 <link rel="stylesheet" href="global/css/green_form.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="global/css/jquery.cleditor.css" type="text/css" media="screen" />
 <?php $this->renderc('template/head-end'); ?>
 <?php $this->renderc('template/nav'); ?>
 <div id="container">
@@ -8,11 +9,12 @@
     <?php $this->renderc($data); ?>
     <div class="content">
       <form id="manage-article-form" class="green-form" action="super_admin/create_user">
-		 
+
 		  <label for="title" class="flat">Title</label>
 		  <input type="text" id="title" name="title" class="title" />
 		  <label for="content">Content</label>
-		  <textarea cols="77" rows="20"></textarea>
+		  <textarea id="content"></textarea>
+
       </form>
 
     </div>
@@ -32,20 +34,26 @@
     </div>
   </div>
   <?php $this->renderc('template/footer'); ?>
-  <script type="text/javascript">
+<script type="text/javascript" src="global/js/jquery.cleditor.min.js"></script>
+ <script type="text/javascript">
+
     $(function(){
       getNavDock();
+       $("#content").cleditor({
+          width:        630,
+          height:       300
+        });
 //      jQuery("#manage-user-form").validationEngine({
 //        ajaxFormValidation: true,
 //        onAjaxFormComplete: ajaxValidationCallback,
 //        onBeforeAjaxFormValidation: beforeCall
 //      });
-    
       $('#loader').remove();
+
 
     }); //end document ready
 
-   
+
   </script>
 </body>
 </html>
